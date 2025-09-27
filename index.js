87f2e95b5e19756a3b7d35fc3f779889108287dd
salehchy46 <salehchyctg@gmail.com>
2025-09-28 03:06:33 +0600

    user data fetch to back end is added

--- index.js
+++ index.js
@@ -25,10 +25,23 @@ async function run() {
     await client.connect();
     const userCollection = client.db('doctorsHouse').collection('userCollection');
 
+    //User related APIs;
+
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
+      //insert email  if user is not in the database;
+      const query = {email: user.email};
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser) {
+        return res.send({message: 'user already exists', insertedId: null})
+      }
       const result = await userCollection.insertOne(user);
-      res.send(result); 
+      res.send(result);
     });
 
     await client.db('admin').command({ ping: 1 });
